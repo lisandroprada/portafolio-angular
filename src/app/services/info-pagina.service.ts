7d5cdb6a56f2d8354fd9a2a93c9e54c46885aa51
Lisandro Prada <lisandro.prada@gmail.com>
2018-09-24 17:59:21 -0300

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,14 +9,25 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) {
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   }
+   private cargarInfo () {
     this.http.get('assets/data/data-pagina.json').subscribe( (resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
-      // console.log( resp['twitter']);
     });
-
    }
+
+   private cargarEquipo () {
+     this.http.get('https://angular-html-29fae.firebaseio.com/equipo.json').subscribe( (resp: any[] ) => {
+      // this.cargada = true;
+      this.equipo = resp;
+     });
+   }
+
 }
