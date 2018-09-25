392e31df52edb456213de7b24946291ed1cbaeb3
Lisandro Prada <lisandro.prada@gmail.com>
2018-09-24 23:02:30 -0300

    final seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,17 +8,57 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient) {
 
     this.cargarProductos();
   }
     private cargarProductos() {
-      this.http.get('https://angular-html-29fae.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: Producto[]) => {
-        // console.log(resp);
-        this.productos = resp;
-        this.cargando = false;
+      return new Promise ( ( resolve, reject ) => {
+        this.http.get('https://angular-html-29fae.firebaseio.com/productos_idx.json')
+        .subscribe( (resp: Producto[]) => {
+          this.productos = resp;
+          this.cargando = false;
+          resolve();
+        });
       });
+
     }
+  getProducto ( id: string) {
+    return this.http.get(`https://angular-html-29fae.firebaseio.com/productos/${ id }.json`);
+  }
+  buscarProducto( termino: string) {
+    if ( this.productos.length === 0 ) {
+      // cargar productos
+      this.cargarProductos().then ( () => {
+      // ejecutar despues de tener los productos
+      // aplicar el filtro
+      this.filtrarProductos( termino );
+      });
+
+    } else {
+      // aplicar el filtro
+      this.filtrarProductos ( termino );
+
+    }
+
+  }
+  private filtrarProductos( termino: string) {
+    // console.log(this.productos);
+    this.productosFiltrado  = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLowerCase();
+
+      if (prod.categoria.indexOf ( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push ( prod );
+      }
+      // console.log(this.productosFiltrado);
+    });
+
+  }
 }
